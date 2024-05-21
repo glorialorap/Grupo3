2e911c1098f05a782bc50e6f36c258cdf665b5c2
JuanCrespoAldana <juancrespoaldana@gmail.com>
2024-05-21 18:04:11 -0500

    Update Articulo.java

--- Articulo.java
+++ Articulo.java
@@ -7,12 +7,18 @@ public class Articulo {
     String descripcion;
     double precioAnterior;
     double precioActual;
+    String marca;  
+    double peso;  
+    String dimensiones;  
     
     public Articulo(int numero, String descripcion, double precioAnterior, double precioActual) {
         this.numero = numero;
         this.descripcion = descripcion;
         this.precioAnterior = precioAnterior;
         this.precioActual = precioActual;
+        this.marca = marca;
+        this.peso = peso;
+        this.dimensiones = dimensiones;
     }
     public int getNumero() {
         return numero;
@@ -37,6 +43,24 @@ public class Articulo {
     }
     public void setPrecioActual(double precioActual) {
         this.precioActual = precioActual;
+    }
+        public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    public String getDimensiones() {
+        return dimensiones;
+    }
+    public void setDimensiones(String dimensiones) {
+        this.dimensiones = dimensiones;
     }
     @Override
     public String toString() {
