64a019dd8cd6eb7889c6c44252f1f238a1164204
JorgeDMS2006 <jorgedmercado2006@gmail.com>
2024-05-21 00:40:18 -0500

    Ejercicio 3 con creacion de archivos

---
+++ GDA.java
@@ -0,0 +1,75 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GDA {
+    List<Articulo> articulos = new ArrayList<>();
+
+    public void crearArchivo(String fileName) {
+        System.out.println("=== Creando el archivo " + fileName + " ===");
+        boolean fileCreated = false;
+        File file = new File(fileName);
+        try {
+            fileCreated = file.createNewFile();
+            FileWriter fileWriter = new FileWriter(file);
+            for (Articulo articulo : articulos) {
+                fileWriter.write(articulo.numero + "," + articulo.descripcion + "," + articulo.precioAnterior + "," + articulo.precioActual + "\n");
+            }
+            fileWriter.close();
+            if (fileCreated) {
+                System.out.println("=== El archivo se creo correctamente ===");
+            } else {
+                System.out.println("=== El archivo ya existe ===");
+            }
+
+        } catch (IOException e) {
+            System.out.println("=== Excepcion al crear el archivo " + e);
+        }
+    }
+
+    public void leerArchivo(String fileName) {
+        System.out.println("=== Leyendo el archivo " + fileName + " ===");
+        File file = new File(fileName);
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String data = sc.nextLine();
+                String[] parts = data.split(",");
+                int numero = Integer.parseInt(parts[0]);
+                String descripcion = parts[1];
+                double precioAnterior = Double.parseDouble(parts[2]);
+                double precioActual = Double.parseDouble(parts[3]);
+                articulos.add(new Articulo(numero, descripcion, precioAnterior, precioActual));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(" === Hubo un error al abrir el archivo " + e);
+        }
+    }
+
+    public void eliminarArticulo(int numero) {
+        for (int i = 0; i < articulos.size(); i++) {
+            if (articulos.get(i).numero == numero) {
+                articulos.remove(i);
+                return;
+            }
+        }
+        System.out.println("=== Artículo no encontrado ===");
+    }
+
+    public void modificarArticulo(int numero, String descripcion, double precioAnterior, double precioActual) {
+        for (Articulo articulo : articulos) {
+            if (articulo.numero == numero) {
+                articulo.descripcion = descripcion;
+                articulo.precioAnterior = precioAnterior;
+                articulo.precioActual = precioActual;
+                return;
+            }
+        }
+        System.out.println("=== Artículo no encontrado ===");
+    }
+    
+}
--- Main.java
+++ Main.java
@@ -1,162 +1,69 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.Comparator;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
-
     public static void main(String[] args) {
-
-        int opcion=0;
         Scanner sc = new Scanner(System.in);
-        
-        Pentagono pentagono = new Pentagono(null, 12, 8.2);
-        Hexagono hexagono = new Hexagono(18);
-        Paralelograma paralelograma = new Paralelograma(null, 4.5, 6, 7.5);
-        Rombo rombo = new Rombo(null, 10, 4.2, 6.8);
+        GDA ga = new GDA();
+        String fileName = "articulos.txt";
+        int opc;
 
-        List<Figura> figuras = new ArrayList<>();
-        figuras.add(pentagono);
-        figuras.add(hexagono);
-        figuras.add(paralelograma);
-        figuras.add(rombo);
-
+        //menu:
         while (true) {
-            System.out.println("\n--- Menú de Operaciones ---");
-            System.out.println("1. Filtrar Figuras por Area");
-            System.out.println("2. Calcular Area Total");
-            System.out.println("3. Calcular Promedio de Perimetros");
-            System.out.println("4. Mostrar Figura con Maximo y Minimo Perimetro y Area");
-            System.out.println("5. Calcular Promedio de Perimetros de Pentagono y Hexagono");
-            System.out.println("6. Salir");
+            System.out.println("\n--BIENVENIDO AL MENU--");
+            System.out.println("[1]Realizar alta\n[2]Realizar baja\n[3]Cambios\n[4]Consultas\n[5]Salir");
+            opc = sc.nextInt();
 
-            System.out.print("\nIngrese una opción: ");
-            opcion = sc.nextInt();
+            switch (opc) {
+                case 1: //altas
+                    System.out.print("Ingrese numero del articulo: ");
+                    int numero=sc.nextInt();
+                    System.out.print("Ingrese descripcion del articulo: ");
+                    String descripcion=sc.next();
+                    System.out.print("Ingrese el precio anterior del articulo: ");
+                    double precioAnterior=sc.nextDouble();
+                    System.out.print("Ingrese el precio actual del articulo: ");
+                    double precioActual=sc.nextDouble();
+                    Articulo nuevoArticulo = new Articulo(numero, descripcion, precioAnterior, precioActual);
+                    ga.articulos.add(nuevoArticulo);
+                    ga.crearArchivo(fileName);
+                    break;
 
-            switch (opcion) {
-                case 1:
-                    filtrarPorArea(figuras, sc);
+                case 2: //bajas
+                    System.out.print("Ingrese el numero del articulo a eliminar: ");
+                    numero=sc.nextInt();
+                    ga.eliminarArticulo(numero);
+                    ga.crearArchivo(fileName);
                     break;
-                case 2:
-                    calcularAreaTotal(figuras);
+
+                case 3: //Cambios
+                    System.out.print("Ingrese el numero del articulo a modificar: ");
+                    numero=sc.nextInt();
+                    System.out.print("Ingrese la nueva descripcion del articulo: ");
+                    descripcion=sc.next();
+                    System.out.print("Ingrese el nuevo precio anterior del articulo: ");
+                    precioAnterior=sc.nextDouble();
+                    System.out.print("Ingrese el nuevo precio actual del articulo: ");
+                    precioActual=sc.nextDouble();
+                    ga.modificarArticulo(numero, descripcion, precioAnterior, precioActual);
+                    ga.crearArchivo(fileName);
                     break;
-                case 3:
-                    calcularPromedioPerimetros(figuras);
+
+                case 4: //consultas
+                    for (Articulo articulo : ga.articulos) {
+                        System.out.println("Numero: " + articulo.numero + ", Descripcion: " + articulo.descripcion + ", Precio Anterior: " + articulo.precioAnterior + ", Precio Actual: " + articulo.precioActual);
+                    }
                     break;
-                case 4:
-                    calcularMaxyMin(figuras);
-                    break;
-                case 5:
-                    calcularPromedioPentagonoHexagono(figuras);
-                    break;
-                case 6:
-                    System.out.println("Saliendo del programa...");
+
+                case 5: //salida
+                    System.out.println("Hasta luego");
                     sc.close();
                     System.exit(0);
                     break;
-                default:
-                    System.out.println("Opcion invalida. Intentelo de nuevo.");
-            }
-            
-        }
-    }
 
-    private static void filtrarPorArea(List<Figura> figuras, Scanner sc){
-        //filtra el area que el usuario desea ver
-            System.out.println("\nCual area desea ver? \n[1]Hexagono [2]Paralelogramo [3]Pentagono [4]Rombo");
-            int areaFigura=sc.nextInt();
-            
-            switch (areaFigura) {
-                case 1:
-                    List<Figura> figurasHexagono = figuras.stream().
-                    filter(f->f instanceof Hexagono).
-                    collect(Collectors.toList());
-                    figurasHexagono.forEach(System.out::println);
-                    break;
-                case 2:
-                    List<Figura> figurasParalelogramo = figuras.stream().
-                    filter(f->f instanceof Paralelograma).
-                    collect(Collectors.toList());
-                    figurasParalelogramo.forEach(System.out::println);
-                    break;
-                case 3:
-                    List<Figura> figurasPentagono = figuras.stream().
-                    filter(f->f instanceof Pentagono).
-                    collect(Collectors.toList());
-                    figurasPentagono.forEach(System.out::println);
-                    break;
-                case 4:
-                    List<Figura> figurasRombo = figuras.stream().
-                    filter(f->f instanceof Rombo).
-                    collect(Collectors.toList());
-                    figurasRombo.forEach(System.out::println);
-                    break;
                 default:
-                System.out.println("Ingresa una opción valida");
+                    System.out.println("Opción no válida. Por favor, elija una opción del menú.");
                     break;
             }
-            
-    }
-    
-    private static void calcularAreaTotal(List<Figura> figuras) {
-        double areaTotal = figuras.stream()
-            .mapToDouble(Figura::Area)
-            .sum();
-        System.out.println("Area total de todas las figuras: " + areaTotal);
-    }
-
-    private static void calcularPromedioPerimetros(List<Figura> figuras) {
-        double promedioPerimetros = figuras.stream()
-            .mapToDouble(Figura::Perimetro)
-            .average()
-            .orElse(0);
-        System.out.println("Promedio de los perimetros de todas las figuras: " + promedioPerimetros);
-    }
-
-    private static void calcularPromedioPentagonoHexagono(List<Figura> figuras) {
-        // Filtrar figuras para obtener solo pentágonos y hexágonos
-        List<Figura> pentagonosYHexagonos = figuras.stream()
-            .filter(figura -> figura instanceof Pentagono || figura instanceof Hexagono)
-            .collect(Collectors.toList());
-
-        // Calcular el promedio de perímetros de los pentágonos y hexágonos
-        double promedioPerimetros = pentagonosYHexagonos.stream()
-            .mapToDouble(Figura::Perimetro)
-            .average()
-            .orElse(0);
-
-        // Mostrar resultado
-        System.out.println("Promedio de perimetros de pentágonos y hexágonos: " + promedioPerimetros);
         }
-    
-
-        private static void calcularMaxyMin(List<Figura> figuras) {
-            // Encontrar la figura con el máximo perímetro y área
-            Figura figuraMaxPerimetro = figuras.stream()
-                    .max(Comparator.comparing(Figura::Perimetro))
-                    .orElse(null);                   
-        
-            Figura figuraMaxArea = figuras.stream()
-                    .max(Comparator.comparing(Figura::Area))
-                    .orElse(null);
-        
-            // Encontrar la figura con el mínimo perímetro y área
-            Figura figuraMinPerimetro = figuras.stream()
-                    .min(Comparator.comparing(Figura::Perimetro))
-                    .orElse(null);
-        
-            Figura figuraMinArea = figuras.stream()
-                    .min(Comparator.comparing(Figura::Area))
-                    .orElse(null);
-        
-            // Mostrar resultados
-            System.out.println("Figura con máximo perímetro: " + figuraMaxPerimetro);
-            System.out.println("Figura con máximo área: " + figuraMaxArea);
-            System.out.println("Figura con mínimo perímetro: " + figuraMinPerimetro);
-            System.out.println("Figura con mínimo área: " + figuraMinArea);
-        }
+    }
 }
-    
-
