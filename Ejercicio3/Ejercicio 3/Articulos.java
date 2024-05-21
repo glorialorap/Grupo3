25df1936ff50f9b610218633fde4f470e94e801c
Brendisb <167158057+Brendisb@users.noreply.github.com>
2024-05-20 23:16:26 -0500

    Clases añadidas

---
+++ Ejercicio3/Ejercicio 3/Articulos.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Articulos {
+    private int numero;
+    private String descripcion;
+    private double precioAnterior;
+    private double precioActual;
+    
+    public Articulos(int numero, String descripcion, double precioAnterior, double precioActual) {
+        this.numero = numero;
+        this.descripcion = descripcion;
+        this.precioAnterior = precioAnterior;
+        this.precioActual = precioActual;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public double getPrecioAnterior() {
+        return precioAnterior;
+    }
+    public void setPrecioAnterior(double precioAnterior) {
+        this.precioAnterior = precioAnterior;
+    }
+    public double getPrecioActual() {
+        return precioActual;
+    }
+    public void setPrecioActual(double precioActual) {
+        this.precioActual = precioActual;
+    }
+    @Override
+    public String toString() {
+        return "Articulos [numero=" + numero + ", descripcion=" + descripcion + ", precioAnterior=" + precioAnterior
+                + ", precioActual=" + precioActual + "]";
+    }
+    
+}
